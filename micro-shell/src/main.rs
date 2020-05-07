b9624d635aff65682506b979ef57b88d6bc3adf1
Dylane-SILVA <62027846+Dylane-SILVA@users.noreply.github.com>
2020-05-07 15:51:33 +0200

    BOUM Ca pipe ici!
    Partie 4 réussie et correction des parties antérieures.

--- micro-shell/src/main.rs
+++ micro-shell/src/main.rs
@@ -1,20 +1,51 @@
+use std::error::Error;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+
+// fn Exo4_pipes(){        
+//         let mut programme1 = Command::new("ls").arg("/")
+//         .stdin(Stdio::piped())
+//         .stdout(Stdio::piped())
+//         .spawn()
+//         .expect("Failed to spawn child process");
+
+        
+//         let entree = programme1.stdin.as_mut().expect("Failed to open stdin");
+//         entree.write_all("Hello, world!".as_bytes()).expect("Failed to write to stdin");
+        
+
+//         let output = programme1.wait_with_output().expect("Failed to read stdout");
+       
+//         //assert_eq!(String::from_utf8_lossy(&output.stdout), "!dlrow ,olleH");
+
+//         let mut programme2 = Command::new("grep").arg("Hello")
+//          .stdin(output)
+//          .spawn();
+
+
+//           assert_eq!("hello",get_stdout(programme2).trim());
+
+//     }
+
+
+fn Exo4_pipesV2(){
+
+    let ls = Command::new("ls")
+    .arg("-alh")
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect(" ls? What is it?");
+
+    let ls_stdout = Stdio::from(ls.stdout.expect("Failed"));
+    let process = Command::new("grep")
+    .arg("Hello")
+    .stdin(ls_stdout)
+    .spawn()
+    .expect("Not hello? something wrong with grep");
+}
+
+
 fn main() -> std::io::Result<()> {
-    //use std::io;
-    use std::process::Command;
-    
-
-    //let mut list_dir = Command::new("ls");
-
-    //list_dir.status().expect("process failed to execute");
-    //println!("status: {}", list_dir.status);
-
-    let status = Command::new("ls")
-                     .status()
-                     .expect("failed to execute process");
-
-    println!("status: {}", status);
-
-    //assert!(status.success());
 
     let _stdin = io::stdin();
     use std::io::{self, Write};
@@ -25,53 +56,23 @@ fn main() -> std::io::Result<()> {
     // c'est mieux que de crash avec un unwrap ou expect ;)
     out.flush()?;
 
+    // On réupère la commande de l'utilisateur
     let mut user_input = String::with_capacity(256);
     _stdin.read_line(&mut user_input)?;
 
-    fn Exo4_pipes(){
-        use std::process::Stdio;
-        
+    
+    // On execute la commande
+    let status = Command::new(user_input.trim())
+                     .status()
+                     .expect("command not found");
 
-        let mut programme1 = Command::new("ls").arg("/")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn child process");
+    println!("status: {}", status);
 
-        
-        let entree = programme1.stdin.as_mut().expect("Failed to open stdin");
-        entree.write_all("Hello, world!".as_bytes()).expect("Failed to write to stdin");
-        
+    //assert!(status.success());
 
-        let output = programme1.wait_with_output().expect("Failed to read stdout");
-       
-        //assert_eq!(String::from_utf8_lossy(&output.stdout), "!dlrow ,olleH");
+    
 
-        let mut programme2 = Command::new("grep").arg("Hello")
-         .stdin(output)
-         .spawn();
-
-
-          assert_eq!("hello",get_stdout(programme2).trim());
-
-    }
-
-    Exo4_pipes();
-        
-    /*
-    fn Exo4_pipes(){
-        use std::process::Stdio;
-
-        let programme1 = Command::new("ls").arg("/")
-        .stdout(Stdio::piped())
-        .output()
-        .expect("Failed to execute command");
-
-        let programme2 = Command::new("grep").arg("Hello")
-         .stdin(programme1)
-         .spawn();
-         //.unwrap_or_else(|e| { panic!("Error to execute process: {}", e) })
-    }*/
+    Exo4_pipesV2();
 
     Ok(())
 
